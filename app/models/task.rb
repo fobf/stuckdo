842edb5fc999840deb328648ea9d4ffeda361dba
Dmitriy K <fobfreeman@gmail.com>
2015-10-02 23:20:39 +0000

    added link detection, image and video

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,11 @@
 class Task < ActiveRecord::Base
   belongs_to :user
   validates :content, presence: true
+  auto_html_for :content do
+    html_escape
+  	image(:width => 200, :height => 250)
+  	youtube(:width => "100%", :height => 250, :autoplay => false)
+  	link :target => "_blank", :rel => "nofollow"
+  	simple_format
+  end
 end
