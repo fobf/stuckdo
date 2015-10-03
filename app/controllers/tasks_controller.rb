caed57535c6a58b4f3eb86ae5241c2e0e1995539
Dmitriy K <fobfreeman@gmail.com>
2015-10-03 21:44:12 +0000

    removed authentification, half done

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,15 +1,16 @@
 class TasksController < ApplicationController
-  before_action :authenticate_user!
+  #before_action :authenticate_user!
   before_action :set_task, only: [:show, :edit, :update, :destroy, :change]
 
   # GET /tasks
   # GET /tasks.json
+   #@to_do = current_user.tasks.where(state: "to_do")
+    #@doing = current_user.tasks.where(state: "doing")
+    #@done = current_user.tasks.where(state: "done")
   def index
-    @to_do = current_user.tasks.where(state: "to_do")
-    @doing = current_user.tasks.where(state: "doing")
-    @done = current_user.tasks.where(state: "done")
-   
-    
+    @to_do = Task.where(state: "to_do")
+    @doing = Task.where(state: "doing")
+    @done = Task.where(state: "done")
   end
 
   # GET /tasks/1
@@ -28,12 +29,13 @@ class TasksController < ApplicationController
 
   # POST /tasks
   # POST /tasks.json
+  #@task = current_user.tasks.new(task_params)
   def create
-   @task = current_user.tasks.new(task_params)
+   @task =Task.new(task_params)
 
     respond_to do |format|
       if @task.save
-        format.html { redirect_to @task, notice: 'Task was successfully created.' }
+        format.html { redirect_to tasks_url, notice: 'Task was successfully created.' }
         format.json { render :show, status: :created, location: @task }
       else
         format.html { render :new }
@@ -47,7 +49,7 @@ class TasksController < ApplicationController
   def update
     respond_to do |format|
       if @task.update(task_params)
-        format.html { redirect_to @task, notice: 'Task was successfully updated.' }
+        format.html { redirect_to tasks_url, notice: 'Task was successfully updated.' }
         format.json { render :show, status: :ok, location: @task }
       else
         format.html { render :edit }
@@ -61,7 +63,7 @@ class TasksController < ApplicationController
   def destroy
     @task.destroy
     respond_to do |format|
-      format.html { redirect_to tasks_url, notice: 'Task was successfully destroyed.' }
+      format.html { redirect_to tasks_path, notice: 'Task was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
